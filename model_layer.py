c1e109a62193e7e301de8d7d44b5b4ee3f7dcde8
yoreurehehee <141827485+yoreurehehee@users.noreply.github.com>
2023-12-07 02:17:58 +0900

    Add files via upload

---
+++ model_layer.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.preprocessing.image import ImageDataGenerator
+
+import os
+import cv2
+import numpy as np
+
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+
+    # Entry block
+    x = layers.Rescaling(1.0 / 255)(inputs)
+    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    activation = "softmax"
+    outputs = layers.Dense(num_classes, activation=activation)(x)
+    return keras.Model(inputs, outputs)
